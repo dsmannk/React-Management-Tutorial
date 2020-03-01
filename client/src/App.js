e4c60ba40b79153aceedbd71cb721239f26a69e8
doosam <dsmannk@naver.com>
2020-03-01 23:59:25 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactComponent as logo } from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
+import axios from 'axios';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -9,6 +10,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import Axios from 'axios';
 
 const styles = theme => ({
   root: {
@@ -19,36 +21,35 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
-
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '이순신',
-    'birthday': '961122',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '장나라',
-    'birthday': '971012',
-    'gender': '여자',
-    'job': '대학생'
-  }
-]
+});
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    // this.callApi()
+    // .then(res => this.setState({customers: res}))
+    // .catch(err => console.log(err))
+    this.callApi();
+  }
+
+  callApi = async () => {
+    //const response = await fetch('/api/customers');
+    //const body = await response.json();
+    //return body;
+    try {
+      const response = await axios.get('/api/customers');
+      this.setState({
+        customers: response.data
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -59,13 +60,14 @@ class App extends Component {
               <TableCell>번호</TableCell>
               <TableCell>이미지</TableCell>
               <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
               <TableCell>성별</TableCell>
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
           {
-            customers.map(c => {
+            this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer
                   key={c.id}
@@ -73,9 +75,10 @@ class App extends Component {
                   image={c.image}
                   name={c.name}
                   birthday={c.gender}
+                  gender={c.gender}
                   job={c.job}/>
               )
-            })
+            }) : ""
           }    
           </TableBody>
         </Table>
